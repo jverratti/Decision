09a464001b54da6aa9a0e7cc66ccb92068cfb50d
jverratti <31357738+jverratti@users.noreply.github.com>
2017-09-10 11:59:41 -0600

    Decision

---
+++ Decision/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Decision
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Type something and press enter");
+           // string userValue;
+          //  userValue = Console.ReadLine();
+          //  Console.WriteLine("You typed: " + userValue);
+         //   Console.ReadLine();
+
+            //whatever user types in will be evaluated
+            //assigns the variable "1" userValue = 1
+            //evaluates the expression ... true or false? if(userValue) == 1)
+            //else evaluates different conditions
+
+            Console.WriteLine("would you prefer what is behind door number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+            if(userValue == "1")
+            {
+                Console.WriteLine("You won a new car");
+                Console.ReadLine();
+            }
+            else if(userValue == "2")
+            {
+                Console.WriteLine("You won a new boat");
+                Console.ReadLine();
+            }
+            else if (userValue == "3")
+            {
+                Console.WriteLine("You won a new cat");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You lose");
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
